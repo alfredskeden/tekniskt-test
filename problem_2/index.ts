78ae8a5bd8d0d197229435778520454d35e648d0
Alfred Skedebäck <alfred.skedeback@softronic.se>
2021-03-02 09:46:53 +0100

    added comments and made options to change browser

--- problem_2/index.ts
+++ problem_2/index.ts
@@ -1,22 +1,25 @@
 import { Builder, By, WebDriver, WebElement } from 'selenium-webdriver';
-
-const getDriver = (): WebDriver => {
-  return new Builder().forBrowser('chrome').build();
+const getDriver = (forBrowser: string): WebDriver => {
+  return new Builder().forBrowser(forBrowser).build();
 }
 
-const checkForLinks = async (webpage: string): Promise<void> => {
-  let driver: WebDriver = getDriver();
+const checkForLinks = async (webpage: string, browser: string): Promise<void> => {
+  let driver: WebDriver = getDriver(browser);
   try {
+      /** navigate to webpage string */
       await driver.get(webpage);
       const elements: Array<WebElement> = await driver.findElements(By.tagName('a'));
       elements.forEach((webElement: WebElement) => {
+        /** getAttribute on WebElement is a promise so wait for it then console.log each link */
         webElement.getAttribute('href').then((hrefString: string) => {
           console.log(hrefString);
         });
       });
   } finally {
+    /** Quits the driver */
     await driver.quit();
   }
 };
 
-checkForLinks('https://www.google.com/');
+/** Url to check and what browser to use, chrome, can change to firefox etc */
+checkForLinks('https://www.google.com/', 'chrome');
